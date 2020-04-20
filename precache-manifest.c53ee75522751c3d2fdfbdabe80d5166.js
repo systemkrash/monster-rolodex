a93880219b9f684c32ce9364ba5e30be52d4b4fb
Melvin Evangelista <melvs.evangelista@gmail.com>
2020-04-20 18:38:22 +0000

    Updates

---
+++ precache-manifest.c53ee75522751c3d2fdfbdabe80d5166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0689138779a107646cd3a71d18cbb6e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a50d6c5edb77ba5bc1f8",
+    "url": "/monster-rolodex/static/css/main.c1709db2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50d6c5edb77ba5bc1f8",
+    "url": "/monster-rolodex/static/js/main.c8a4c0d6.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
